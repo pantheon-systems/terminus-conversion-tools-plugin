5de19fbc82ac87d0ab219da7ff21c0a8d10828fa
Sergei Churilo <sergei.churilo@pantheon.io>
2021-10-01 15:26:00 +0300

    Add site requirements: Drupal8 framework and drops-8 upstream

--- src/Commands/ConvertToComposerSiteCommand.php
+++ src/Commands/ConvertToComposerSiteCommand.php
@@ -3,21 +3,42 @@
 namespace Pantheon\TerminusConversionTools\Commands;
 
 use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\Terminus\Exceptions\TerminusException;
+use Pantheon\Terminus\Site\SiteAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareTrait;
 
 /**
  * Class ConvertToComposerSiteCommand.
  */
-class ConvertToComposerSiteCommand extends TerminusCommand
+class ConvertToComposerSiteCommand extends TerminusCommand implements SiteAwareInterface
 {
+    use SiteAwareTrait;
+
     /**
      * Convert a standard Drupal8 site into a Drupal8 site managed by Composer.
      *
      * @command conversion:composer
      *
      * @param string $site_id
+     *
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
      */
     public function convert(string $site_id)
     {
-        $this->log()->notice('Hello world, {site_id}!', ['site_id' => $site_id]);
+        $site = $this->getSite($site_id);
+
+        if (!$site->isDrupal8Framework()) {
+            throw new TerminusException(
+                'The site {site_name} is not a Drupal 8 based site.',
+                ['site_name' => $site->getName()]
+            );
+        }
+
+        if (!$site->getUpstream()->isDrops8Upstream()) {
+            throw new TerminusException(
+                'The site {site_name} is not a "drops-8" upstream based site.',
+                ['site_name' => $site->getName()]
+            );
+        }
     }
 }
