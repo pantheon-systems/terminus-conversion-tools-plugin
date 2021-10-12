491babb0467f3d3d97e3baa5229d1daa93ea0a86
Sergei Churilo <sergei.churilo@pantheon.io>
2021-10-12 10:16:54 +0300

    Update Composer to use Symfony Process component instead of CommandExecutorTrait

--- src/Utils/Composer.php
+++ src/Utils/Composer.php
@@ -3,15 +3,14 @@
 namespace Pantheon\TerminusConversionTools\Utils;
 
 use Pantheon\Terminus\Exceptions\TerminusException;
-use Pantheon\Terminus\Helpers\Traits\CommandExecutorTrait;
+use Symfony\Component\Process\Process;
+use Throwable;
 
 /**
  * Class Composer.
  */
 class Composer
 {
-    use CommandExecutorTrait;
-
     /**
      * @var string
      */
@@ -48,6 +47,25 @@ class Composer
      */
     public function require(string $package, string $version): void
     {
-        $this->execute(sprintf('composer require %s:%s --working-dir=%s', $package, $version, $this->workingDirectory));
+        $this->execute(['composer', 'require', sprintf('%s:%s', $package, $version)]);
+    }
+
+    /**
+     * Executes the Composer command.
+     *
+     * @param array $command
+     *
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
+     */
+    private function execute(array $command): void
+    {
+        try {
+            $process = new Process($command, $this->workingDirectory);
+            $process->mustRun();
+        } catch (Throwable $t) {
+            throw new TerminusException(
+                sprintf('Failed executing Composer command: %s', $t->getMessage())
+            );
+        }
     }
 }
