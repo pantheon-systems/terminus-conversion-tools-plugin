7bc579a65053b3dee1bef6fba34814ec1965fa86
Sergei Churilo <sergei.churilo@pantheon.io>
2021-10-20 13:18:51 +0300

    Use ConversionCommandsTrait in ReleaseComposerifyToMasterCommand

--- src/Commands/ReleaseComposerifyToMasterCommand.php
+++ src/Commands/ReleaseComposerifyToMasterCommand.php
@@ -5,12 +5,9 @@ namespace Pantheon\TerminusConversionTools\Commands;
 use Pantheon\Terminus\Commands\TerminusCommand;
 use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
 use Pantheon\Terminus\Exceptions\TerminusException;
-use Pantheon\Terminus\Helpers\LocalMachineHelper;
-use Pantheon\Terminus\Models\Environment;
-use Pantheon\Terminus\Models\Site;
-use Pantheon\Terminus\Models\TerminusModel;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
+use Pantheon\TerminusConversionTools\Commands\Traits\ConversionCommandsTrait;
 use Pantheon\TerminusConversionTools\Utils\Git;
 
 /**
@@ -20,6 +17,7 @@ class ReleaseComposerifyToMasterCommand extends TerminusCommand implements SiteA
 {
     use SiteAwareTrait;
     use WorkflowProcessingTrait;
+    use ConversionCommandsTrait;
 
     private const COMPOSERIFY_GIT_BRANCH = 'composerify';
     private const BACKUP_GIT_BRANCH = 'master-bckp';
@@ -52,7 +50,6 @@ class ReleaseComposerifyToMasterCommand extends TerminusCommand implements SiteA
 
         $localPath = $this->cloneSiteGitRepository(
             $site,
-            $devEnv,
             sprintf('%s_composer_conversion', $site->getName())
         );
 
@@ -65,8 +62,8 @@ class ReleaseComposerifyToMasterCommand extends TerminusCommand implements SiteA
             $masterBranchHeadCommitHash = $git->getHeadCommitHash(self::MASTER_GIT_BRANCH);
             $this->log()->notice(
                 sprintf(
-                    'Creating backup multidev environment "%s" from "master" ("%s")...',
-                    self::BACKUP_GIT_BRANCH,
+                    'Creating backup of "%s" ("%s" commit)...',
+                    self::MASTER_GIT_BRANCH,
                     $masterBranchHeadCommitHash
                 )
             );
@@ -82,7 +79,7 @@ class ReleaseComposerifyToMasterCommand extends TerminusCommand implements SiteA
             );
         }
 
-        $this->log()->notice(sprintf('Replacing "master" with "%s" git branch ...', $sourceBranch));
+        $this->log()->notice(sprintf('Replacing "%s" with "%s" git branch...', self::MASTER_GIT_BRANCH, $sourceBranch));
         $git->checkout(self::MASTER_GIT_BRANCH);
         $git->reset('--hard', $git->getHeadCommitHash($sourceBranch));
         $git->push(self::MASTER_GIT_BRANCH, '--force');
@@ -92,77 +89,5 @@ class ReleaseComposerifyToMasterCommand extends TerminusCommand implements SiteA
         $this->processWorkflow($site->setUpstream($upstream->id));
 
         $this->log()->notice(sprintf('Link to "dev" environment dashboard: %s', $devEnv->dashboardUrl()));
-    }
-
-    /**
-     * Clones the site repository to local machine and return the absolute path to the local copy.
-     *
-     * @param \Pantheon\Terminus\Models\Site $site
-     * @param \Pantheon\Terminus\Models\Environment $env
-     * @param $siteDirName
-     *
-     * @return string
-     *
-     * @throws \Pantheon\Terminus\Exceptions\TerminusAlreadyExistsException
-     * @throws \Pantheon\Terminus\Exceptions\TerminusException
-     *
-     * @todo: remove code duplicates
-     */
-    private function cloneSiteGitRepository(Site $site, Environment $env, $siteDirName): string
-    {
-        $path = $site->getLocalCopyDir($siteDirName);
-        $this->log()->notice(
-            sprintf('Cloning %s site repository into "%s"...', $site->getName(), $path)
-        );
-        $gitUrl = $env->connectionInfo()['git_url'] ?? null;
-        $this->getLocalMachineHelper()->cloneGitRepository($gitUrl, $path, true);
-
-        return $path;
-    }
-
-    /**
-     * Returns the LocalMachineHelper.
-     *
-     * @return \Pantheon\Terminus\Helpers\LocalMachineHelper
-     *
-     * @todo: remove code duplicates
-     */
-    private function getLocalMachineHelper(): LocalMachineHelper
-    {
-        if (isset($this->localMachineHelper)) {
-            return $this->localMachineHelper;
-        }
-
-        $this->localMachineHelper = $this->getContainer()->get(LocalMachineHelper::class);
-
-        return $this->localMachineHelper;
-    }
-
-
-    /**
-     * Creates the target multidev environment.
-     *
-     * @param \Pantheon\Terminus\Models\Site $site
-     * @param string $branch
-     *
-     * @return \Pantheon\Terminus\Models\Environment
-     *
-     * @throws \Pantheon\Terminus\Exceptions\TerminusException
-     * @throws \Pantheon\Terminus\Exceptions\TerminusNotFoundException
-     *
-     * @todo remove code duplicates
-     */
-    private function createMultidev(Site $site, string $branch): TerminusModel
-    {
-        $this->log()->notice(sprintf('Creating "%s" multidev environment...', $branch));
-
-        /** @var \Pantheon\Terminus\Models\Environment $devEnv */
-        $devEnv = $site->getEnvironments()->get('dev');
-
-        $workflow = $site->getEnvironments()->create($branch, $devEnv);
-        $this->processWorkflow($workflow);
-        $site->unsetEnvironments();
-
-        return $site->getEnvironments()->get($branch);
     }
 }
