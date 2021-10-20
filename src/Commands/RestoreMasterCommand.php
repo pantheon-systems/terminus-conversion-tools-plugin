1a999f6964dc578329cdf9809c8907c7ff096ec2
Sergei Churilo <sergei.churilo@pantheon.io>
2021-10-20 19:02:36 +0300

    Update RestoreMasterCommand

--- src/Commands/RestoreMasterCommand.php
+++ src/Commands/RestoreMasterCommand.php
@@ -4,6 +4,7 @@ namespace Pantheon\TerminusConversionTools\Commands;
 
 use Pantheon\Terminus\Commands\TerminusCommand;
 use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
+use Pantheon\Terminus\Exceptions\TerminusException;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
 use Pantheon\TerminusConversionTools\Commands\Traits\ConversionCommandsTrait;
@@ -35,13 +36,40 @@ class RestoreMasterCommand extends TerminusCommand implements SiteAwareInterface
     {
         $site = $this->getSite($site_id);
 
+        /** @var \Pantheon\Terminus\Models\Environment $devEnv */
+        $devEnv = $site->getEnvironments()->get('dev');
+
         $localPath = $this->cloneSiteGitRepository(
             $site,
             sprintf('%s_composer_conversion', $site->getName())
         );
 
         $git = new Git($localPath);
+        if (!$git->isRemoteBranchExists(self::BACKUP_GIT_BRANCH)) {
+            throw new TerminusException(sprintf('The backup branch "%s" does not exist', self::BACKUP_GIT_BRANCH));
+        }
 
-        // @todo: restore master from the backup branch.
+        $backupMasterCommitHash = $git->getHeadCommitHash(self::BACKUP_GIT_BRANCH);
+        if (!$this->input()->getOption('yes') && !$this->io()
+                ->confirm(
+                    sprintf(
+                        'Are you sure you want to restore "%s" git branch to "%s"?',
+                        self::MASTER_GIT_BRANCH,
+                        $backupMasterCommitHash
+                    ),
+                )
+        ) {
+            return;
+        }
+
+        $git->checkout(self::MASTER_GIT_BRANCH);
+        $git->reset('--hard', $backupMasterCommitHash);
+        $git->push(self::MASTER_GIT_BRANCH, '--force');
+
+        $this->log()->notice(sprintf('Changing the upstream to "%s"...', self::DROPS_8_UPSTREAM_ID));
+        $upstream = $this->session()->getUser()->getUpstreams()->get(self::DROPS_8_UPSTREAM_ID);
+        $this->processWorkflow($site->setUpstream($upstream->id));
+
+        $this->log()->notice(sprintf('Link to "dev" environment dashboard: %s', $devEnv->dashboardUrl()));
     }
 }
