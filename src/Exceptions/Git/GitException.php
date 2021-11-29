5022fc520dc41abec8ff4a0ee51d4ead662b4513
Sergei Churilo <sergei.churilo@pantheon.io>
2021-11-29 11:04:39 +0300

    Add Git exceptions

---
+++ src/Exceptions/Git/GitException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pantheon\TerminusConversionTools\Exceptions\Git;
+
+use Exception;
+
+/**
+ * Class GitException.
+ *
+ * @package Pantheon\TerminusConversionTools\Exceptions\Git
+ */
+class GitException extends Exception
+{
+}
---
+++ src/Exceptions/Git/GitNoDiffException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pantheon\TerminusConversionTools\Exceptions\Git;
+
+/**
+ * Class TerminusCancelOperationException.
+ *
+ * @package Pantheon\TerminusConversionTools\Exceptions\Git
+ */
+class GitNoDiffException extends GitException
+{
+}
