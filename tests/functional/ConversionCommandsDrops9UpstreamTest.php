947ea27988281883a177942a738a20792999d9f0
Kevin Porras <kevin.porras@pantheon.io>
2022-02-18 13:28:56 -0600

    Initial attempt to add tests using d9 as source.

---
+++ tests/functional/ConversionCommandsDrops9UpstreamTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Pantheon\TerminusConversionTools\Tests\Functional;
+
+/**
+ * Class ConversionCommandsDrops9UpstreamTest.
+ *
+ * Uses a site fixture based on https://github.com/pantheon-fixtures/site-drupal9-non-composer custom upstream.
+ *
+ * @package Pantheon\TerminusConversionTools\Tests\Functional
+ */
+final class ConversionCommandsDrops9UpstreamTest extends ConversionCommandsUpstreamTestBase
+{
+    /**
+     * @inheritdoc
+     */
+    protected function getUpstreamIdEnvName(): string
+    {
+        return 'TERMINUS_TEST_SITE_DROPS9_UPSTREAM_ID';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getRealUpstreamId(): string
+    {
+        return 'drupal8';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedAdviceBeforeConversion(): string
+    {
+        return 'convert the site to a Composer managed one by using `conversion:composer` Terminus command';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedAdviceAfterConversion(): string
+    {
+        return 'No conversion is necessary.';
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @group upstream_drops9
+     */
+    public function testConversionCommands(): void
+    {
+        parent::testConversionCommands();
+    }
+}
