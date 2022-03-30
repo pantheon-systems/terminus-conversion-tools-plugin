d3a5c79499f868ea044cdb8dea9695dfc9c86bd9
Sergei Churilo <sergei.churilo@pantheon.io>
2022-03-30 14:34:42 +0400

    Ignore "vendor" dir after .gitignore created

--- src/Commands/ValidateAndFixGitignoreCommand.php
+++ src/Commands/ValidateAndFixGitignoreCommand.php
@@ -9,6 +9,7 @@ use Pantheon\TerminusConversionTools\Commands\Traits\ComposerAwareTrait;
 use Pantheon\TerminusConversionTools\Commands\Traits\ConversionCommandsTrait;
 use Pantheon\TerminusConversionTools\Exceptions\TerminusCancelOperationException;
 use Pantheon\TerminusConversionTools\Utils\Files;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class ValidateAndFixGitignoreCommand.
@@ -34,19 +35,20 @@ class ValidateAndFixGitignoreCommand extends TerminusCommand implements SiteAwar
      * @throws \Pantheon\TerminusConversionTools\Exceptions\TerminusCancelOperationException
      * @throws \Pantheon\Terminus\Exceptions\TerminusException
      * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Pantheon\TerminusConversionTools\Exceptions\Git\GitException
      */
     public function validateAndFixGitignore(string $site_id): void
     {
         $this->setSite($site_id);
 
-        $this->gitignoreFilePath = Files::buildPath($this->getLocalSitePath(), '.gitignore');
-        $this->validateGitignoreExists();
-
-        $this->setGit($this->getLocalSitePath());
-
         $this->setComposer($this->getLocalSitePath());
         $this->log()->notice('Installing Composer dependencies...');
         $this->getComposer()->install();
+
+        $this->gitignoreFilePath = Files::buildPath($this->getLocalSitePath(), '.gitignore');
+        $this->setGit($this->getLocalSitePath());
+        $this->validateGitignoreExists();
+
         $installationPaths = $this->getComposer()->getInstallationPaths();
 
         // 1. clone site
@@ -54,8 +56,9 @@ class ValidateAndFixGitignoreCommand extends TerminusCommand implements SiteAwar
         // 2. check for existing .gitignore file
         // 3. install dependencies
         // 4. analyze
-        //    - "vendor" must be always ignored
+        //    - get Composer installation paths
         // 5. suggest fixes
+        //    - "vendor" must be always ignored
         // 6. confirm fixes
         // 7. apply fixes
 
@@ -65,6 +68,7 @@ class ValidateAndFixGitignoreCommand extends TerminusCommand implements SiteAwar
     /**
      * Validates the .gitignore file exists, otherwise suggests creating it automatically.
      *
+     * @throws \Pantheon\TerminusConversionTools\Exceptions\Git\GitException
      * @throws \Pantheon\TerminusConversionTools\Exceptions\TerminusCancelOperationException
      * @throws \Pantheon\Terminus\Exceptions\TerminusException
      * @throws \Psr\Container\ContainerExceptionInterface
@@ -78,7 +82,7 @@ class ValidateAndFixGitignoreCommand extends TerminusCommand implements SiteAwar
         if (!$this->input()->getOption('yes')
             && !$this->io()->confirm(
                 sprintf(
-                    'File .gitignore not found in "%s". Do you want to create it?',
+                    'File .gitignore not found in "%s". Do you want to create and commit it?',
                     $this->getLocalSitePath()
                 )
             )
@@ -91,10 +95,41 @@ class ValidateAndFixGitignoreCommand extends TerminusCommand implements SiteAwar
             );
         }
 
-        if (!file_put_contents($this->gitignoreFilePath, '# Created by Terminus Conversion Tools Plugin.' . PHP_EOL)) {
+        if (!file_put_contents($this->gitignoreFilePath, '# Added by Terminus Conversion Tools Plugin.' . PHP_EOL)) {
             throw new TerminusException(
                 sprintf('Failed to create .gitignore file in "%s".', $this->getLocalSitePath())
             );
         }
+        $this->getGit()->commit('Add .gitignore', ['.gitignore']);
+
+        $fs = new Filesystem();
+        $defaultPathsToIgnore = array_filter(
+            ['vendor'],
+            fn($path) => $fs->exists($path)
+        );
+
+        foreach ($defaultPathsToIgnore as $path) {
+            $this->addPathToIgnore($path);
+        }
+    }
+
+    /**
+     * Adds a path to .gitignore.
+     *
+     * @param string $path
+     *
+     * @throws \Pantheon\TerminusConversionTools\Exceptions\Git\GitException
+     */
+    private function addPathToIgnore(string $path): void
+    {
+        $this->log()->notice(sprintf('Adding "%s" to .gitignore...', $path));
+
+        $gitignoreFile = fopen($this->gitignoreFilePath, 'a');
+        fwrite($gitignoreFile, $path);
+        fclose($gitignoreFile);
+        $this->getGit()->commit(sprintf('Add "%s" to .gitignore', $path), ['.gitignore']);
+
+        $this->getGit()->remove('-r', '--cached', $path);
+        $this->getGit()->commit(sprintf('Remove ignored path "%s"', $path), ['-u']);
     }
 }
