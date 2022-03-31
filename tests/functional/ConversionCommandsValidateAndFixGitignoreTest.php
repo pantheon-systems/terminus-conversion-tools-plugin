317612bf4268eedf9e816185d5f25defa50c74c8
Sergei Churilo <sergei.churilo@pantheon.io>
2022-03-31 21:25:02 +0400

    Add testing scenarios for ConversionCommandsValidateAndFixGitignoreTest

--- tests/functional/ConversionCommandsValidateAndFixGitignoreTest.php
+++ tests/functional/ConversionCommandsValidateAndFixGitignoreTest.php
@@ -3,9 +3,13 @@
 namespace Pantheon\TerminusConversionTools\Tests\Functional;
 
 use Pantheon\TerminusConversionTools\Utils\Files;
+use Pantheon\TerminusConversionTools\Utils\Git;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class ConversionCommandsValidateAndFixGitignoreTest.
+ *
+ * Uses a site fixture based on https://github.com/pantheon-fixtures/site-non-ic custom upstream.
  *
  * @package Pantheon\TerminusConversionTools\Tests\Functional
  */
@@ -23,24 +27,28 @@ final class ConversionCommandsValidateAndFixGitignoreTest extends ConversionComm
      * @inheritdoc
      *
      * @group validate_and_fix_gitignore_command
+     *
+     * @throws \Pantheon\TerminusConversionTools\Exceptions\Git\GitException
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
      */
     public function testConversionCommands(): void
     {
-        $this->terminus(sprintf('conversion:validate-gitignore %s', $this->siteName));
-
         $localSiteDirName = sprintf('%s_terminus_conversion_plugin', $this->siteName);
-        $localSitePath = $gitignoreFilePath = Files::buildPath(
+        $localSitePath = Files::buildPath(
             $_SERVER['HOME'],
             'pantheon-local-copies',
             $localSiteDirName
         );
-        $gitignoreFilePath = Files::buildPath($localSitePath, '.gitignore');
 
+        // Execute the command.
+        $this->terminus(sprintf('conversion:validate-gitignore %s', $this->siteName));
+
+        // Verify .gitignore file.
+        $gitignoreFilePath = Files::buildPath($localSitePath, '.gitignore');
         $this->assertTrue(
             is_file($gitignoreFilePath),
             sprintf('File "%s" must exist', $gitignoreFilePath)
         );
-
         $this->assertEquals(
             <<<EOD
 # Added by Terminus Conversion Tools Plugin.
@@ -52,6 +60,29 @@ final class ConversionCommandsValidateAndFixGitignoreTest extends ConversionComm
 
 EOD,
             file_get_contents($gitignoreFilePath)
+        );
+
+        $git = new Git($localSitePath);
+        $fs = new Filesystem();
+
+        // Before deleting Composer-installed assets.
+        $this->assertEmpty($git->diffFileList(), 'Git diff result must be empty.');
+
+        // After deleting some Composer-installed assets.
+        $fs->remove(Files::buildPath($localSitePath, 'vendor'));
+        $fs->remove(Files::buildPath($localSitePath, 'web/core'));
+        $fs->remove(Files::buildPath($localSitePath, 'web/modules/composer'));
+        $this->assertEmpty(
+            $git->diffFileList(),
+            'Git diff result must be empty after deleting Composer-installed assets.'
+        );
+
+        // After deleting some Composer-installed assets and a non-gitignored test file.
+        $fs->remove(Files::buildPath($localSitePath, 'web/themes/composer'));
+        $this->assertEquals(
+            ['web/themes/composer/this_file_must_not_be_gitignored.txt'],
+            $git->diffFileList(),
+            'Git diff must have exactly one deleted file "web/themes/composer/this_file_must_not_be_gitignored.txt"'
         );
     }
 }
