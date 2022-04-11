23bd7c018000df042de040dcfb38c04904954ebb
Kevin Porras <kevin.porras@pantheon.io>
2022-04-11 15:33:20 -0600

    Modify existing test to include upgrade-d9.

--- tests/functional/ConversionCommandsDrops8UpstreamTest.php
+++ tests/functional/ConversionCommandsDrops8UpstreamTest.php
@@ -51,9 +51,43 @@ final class ConversionCommandsDrops8UpstreamTest extends ConversionCommandsUpstr
      * @group release_to_master_command
      * @group restore_master_command
      * @group advise_command
+     * @group upgrade_d9_command
      */
     public function testConversionCommands(): void
     {
-        parent::testConversionCommands();
+        $this->assertPagesExists(self::DEV_ENV);
+
+        $this->assertAdviseBeforeCommand();
+        $this->executeConvertCommand();
+
+        $this->assertCommand(
+            sprintf('conversion:release-to-master %s --branch=%s', $this->siteName, $this->branch),
+            $this->branch
+        );
+        $siteInfoUpstream = $this->terminusJsonResponse(sprintf('site:info %s', $this->siteName))['upstream'];
+        $this->assertEquals(
+            '897fdf15-992e-4fa1-beab-89e2b5027e03: https://github.com/pantheon-upstreams/drupal-recommended',
+            $siteInfoUpstream
+        );
+        $this->assertAdviseAfterCommand();
+
+        // Delete multidev env so that we can cleanly continue with the D9 upgrade.
+        $this->terminus(
+            sprintf('multidev:delete %s.conversion --delete-branch', $this->siteName),
+            ['--quiet'],
+            false
+        );
+
+        $this->assertCommand(
+            sprintf('conversion:upgrade-d9 --skip-upgrade-status %s --branch=%s', $this->siteName, $this->branch),
+            $this->branch
+        );
+
+        $this->assertCommand(
+            sprintf('conversion:restore-master %s', $this->siteName),
+            self::DEV_ENV
+        );
+        $siteInfoUpstream = $this->terminusJsonResponse(sprintf('site:info %s', $this->siteName))['upstream'];
+        $this->assertEquals($this->expectedSiteInfoUpstream, $siteInfoUpstream);
     }
 }
